44e689a60b574d9f73d047feac778269045f957e
vijayshankar199 <117157165+vijayshankar199@users.noreply.github.com>
2025-11-27 18:54:39 +0530

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,18 +1,23 @@
-create database restaurant_db;
-use restaurant_db;
-CREATE TABLE admin(
+
+CREATE DATABASE IF NOT EXISTS restaurant_db;
+
+
+USE restaurant_db;
+
+CREATE TABLE IF NOT EXISTS admin (
     username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(50)
 );
 
-CREATE TABLE menu(
+
+CREATE TABLE IF NOT EXISTS menu (
     item_id INT AUTO_INCREMENT PRIMARY KEY,
     item_name VARCHAR(100),
     category VARCHAR(50),
     price FLOAT
 );
 
-CREATE TABLE orders(
+CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     user_name VARCHAR(100),
     mobile_no VARCHAR(20),
@@ -20,7 +25,9 @@ CREATE TABLE orders(
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE order_items(
+
+
+CREATE TABLE IF NOT EXISTS order_items (
     id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     item_id INT,
@@ -29,3 +36,9 @@ CREATE TABLE order_items(
     FOREIGN KEY(order_id) REFERENCES orders(order_id),
     FOREIGN KEY(item_id) REFERENCES menu(item_id)
 );
+
+
+INSERT INTO admin (username, password)
+VALUES ('vijay', 'Vijay@123');
+
+select * from admin;
